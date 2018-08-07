6c54c9d5d235c19766ff901c63b6084a521e0c67
Diego Esteves <diegofelipece@gmail.com>
2018-08-07 09:48:55 -0300

    Create dist folder and improve demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,37 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
+const paths = {
+  app: {
+    sass: './scss/**/*.scss',
+    css: './dist'
+  },
+  demo: {
+    sass: './demo/scss/**/*.scss',
+    css: './demo/css'
+  }
+}
+
 gulp.task('sass', function () {
-  return gulp.src('./scss/**/*.scss')
+  return gulp.src(paths.app.sass)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest(paths.app.css));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch(paths.app.sass, ['sass']);
 });
+
+gulp.task('demo', function () {
+  return gulp.src(paths.demo.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.demo.css));
+});
+
+gulp.task('demo:watch', function () {
+  gulp.watch(paths.demo.sass, ['demo']);
+});
+
+gulp.task('dev', ['sass', 'sass:watch', 'demo:watch']);
 
 gulp.task('default', ['sass', 'sass:watch']);
