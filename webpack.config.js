ed707762531c882ddec225c2182f2a15f3f107bf
Diego Esteves <diegofelipece@gmail.com>
2020-04-09 11:42:54 +0200

    chore: improve demo with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,21 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const isDevMode = process.env.NODE_ENV !== 'production';
 
 const DIR = {
-  DEMO: path.resolve('./demo'),
-  SASS: path.resolve('./scss'),
-  DIST: path.resolve('./dist'),
+  DEMO: path.resolve(__dirname, './demo'),
+  SASS: path.resolve(__dirname, './scss'),
+  DIST: path.resolve(__dirname, './dist'),
 }
 
 const config = {
-  mode: isDevMode ? 'development' : 'production',
-  entry: './hourglass.js',
+  mode: 'production',
+  entry: [
+    `${DIR.SASS}/hourglass.scss`,
+  ],
   output: {
     filename: isDevMode ? 'hourglass.js' : 'hourglass.min.js',
   },
@@ -28,12 +30,8 @@ const config = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
-          // not sass-loader
           MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS
           'sass-loader',
         ],
       },
@@ -42,13 +40,25 @@ const config = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: isDevMode ? 'hourglass.css' : 'hourglass.min.css',
-    }),
-  ],
-  // devServer: {
-  //   contentBase: DIR.DEMO,
-  //   compress: true,
-  //   port: 8080
-  // }
+    })
+  ]
+}
+
+if (isDevMode) {
+  config.mode = 'development';
+
+  config.entry.push(`${DIR.DEMO}/scss/style.scss`)
+
+  config.plugins.push(new HtmlWebpackPlugin({
+    path: DIR.DEMO,
+    template: `${DIR.DEMO}/index.html`
+  }));
+
+  config.devServer = {
+    contentBase: DIR.DEMO,
+    compress: true,
+    port: 8080
+  };
 }
 
 module.exports = config;
