af54b82c8f2ab794922e9f74a91391e115996a12
Diego Esteves <diegofelipece@gmail.com>
2020-04-09 10:35:50 +0200

    chore: add basic webpack config

---
+++ hourglass.js
@@ -0,0 +1,3 @@
+import hourglass from './scss/hourglass.scss';
+
+export default hourglass;
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,45 @@
-const path = require('path')
+const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const DEMO_DIR = path.resolve(__dirname, 'demo')
-
-const config = {
-  devServer: {
-    contentBase: DEMO_DIR,
-    compress: true,
-    port: 8080
-  },
+const DIR = {
+  DEMO_DIR: path.resolve('demo'),
+  SASS: path.resolve('scss'),
+  DIST: path.resolve('dist'),
 }
 
-module.exports = config
+module.exports = {
+  mode: 'development',
+  entry: './hourglass.js',
+  output: {
+    filename: 'hourglass.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          // not sass-loader
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'hourglass.css',
+    }),
+  ],
+  // devServer: {
+  //   contentBase: DIR.DEMO,
+  //   compress: true,
+  //   port: 8080
+  // }
+}
